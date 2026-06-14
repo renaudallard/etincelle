13f934c86f1bf8ece61de023fc2b002c94fb2199
Renaud Allard <renaud@allard.it>
2026-06-14 16:52:19 +0200

    Bump version to 0.0.3

--- app-mobile/build.gradle.kts
+++ app-mobile/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "it.allard.etincelle"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
     }
 
     signingConfigs {
