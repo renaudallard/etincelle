819b641340afb7f38404f9e7644a07589a2d0f4a
Renaud Allard <renaud@allard.it>
2026-06-15 12:45:36 +0200

    Bump version to 0.0.8

--- app-mobile/build.gradle.kts
+++ app-mobile/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "it.allard.etincelle"
         minSdk = 23
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
     }
 
     signingConfigs {
