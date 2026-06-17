566bd8f8028cacdf5fe6079e64b55cb3f4bd42b2
Renaud Allard <renaud@allard.it>
2026-06-17 20:17:20 +0200

    Bump version to 0.1.11

--- app-tv/build.gradle.kts
+++ app-tv/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "it.allard.etincelle.tv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.1.10"
+        versionCode = 23
+        versionName = "0.1.11"
     }
 
     signingConfigs {
